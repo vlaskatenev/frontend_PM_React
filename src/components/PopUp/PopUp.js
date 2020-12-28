6eba9af702cbbe854d011358c0293f92ef3d836e
vlaskatenev <hiperst35@gmail.com>
2020-12-29 00:55:18 +0300

    html added and function from give json data from server

--- src/components/PopUp/PopUp.js
+++ src/components/PopUp/PopUp.js
@@ -1,14 +1,20 @@
 import React from "react";
 import './PopUp.css'
 
-const RenderPopUp = ({active, setActive, content}) => {
+const RenderPopUp = ({active, setActive, content, setCompList}) => {
     return (
-        <div className={active ? 'modal active' : 'modal' } onClick={() => setActive(false)}>
+        <div className={active ? 'modal active' : 'modal' } onClick={() => changeState(setActive, setCompList)}>
             <div className='modal__content' onClick={e => e.stopPropagation()}>
-                {content()}
+                {content}
             </div>
         </div>
     )
 }
 
 export default RenderPopUp
+
+
+const changeState = (setActive, setCompList) => {
+    setActive(false)
+    setCompList([])
+}
--- src/containers/InstallSoft/InstallSoft.js
+++ src/containers/InstallSoft/InstallSoft.js
@@ -1,38 +1,83 @@
 import React, {useState} from 'react'
 import RenderPopUp from '../../components/PopUp/PopUp'
 import './InstallSoft.css'
+import {axiosPost} from "../../axios/axiosMethods"
+
 
 const InstallSoft = () => {
 
-        const [modalActive, setModalActive] = useState(false)
+const [modalActive, setModalActive] = useState(false)
+const [objFromAD, setobjFromAD] = useState(false)
+const [compList, setCompList] = useState([])
 
-        return (
-            <div className="InstallSoft">
-                <div className="input"
-                     contentEditable="true"
-                     spellCheck="false"
-                     data-set="input"
-                >
-                </div>
-                <div className="button">
-                    <i className="material-icons" data-set="toStart">
-                        flip_camera_android</i>
-                </div>
-                <button onClick={() => {setModalActive(true)}}>Показать попап окно</button>
 
-                <RenderPopUp active={modalActive} setActive={setModalActive} content={contentForPopUp}/>
-            </div>
-        )
-    }
+		return (
+				<div className="InstallSoft">
+						<div><p>Введи имя ПК если нужна установка на один компьютер:</p></div>
+						<div className="inputForm">
+						<div className="input"
+									contentEditable="true"
+									spellCheck="false"
+									data-set="input"
+						>
+						</div>
+						<div className="button">
+								<i className="material-icons" data-set="toStart">
+										flip_camera_android</i>
+						</div>
+						</div>
+						<div>
+								<button onClick={() => {listNamePc(setModalActive, setobjFromAD)}}>Выбрать ПК</button>
+						</div>
 
-const contentForPopUp = () => {
+						{ modalActive 
+							? <RenderPopUp active={modalActive} 
+															setActive={setModalActive} 
+															content={contentForPopUp(objFromAD, addedCompToList)}
+															setCompList={setCompList}/> 
+							: '' }
+				</div>
+		)
+}
+		
+export default InstallSoft
+
+const addedCompToList = compName => {
+		compList.indexOf( compName ) === -1 
+			? compList.push(compName) 
+			: compList.splice(compList.indexOf(compName), 1)
+		console.log(compList);
+}
+
+
+const listNamePc = async (setModalActive, setobjFromAD) => {  
+    const data = await axiosPost('/list-computers', {ad_tree: "OU=comps,DC=npr,DC=nornick,DC=ru"})
+    console.log(data.data.data.computerName[0])
+    setobjFromAD(data.data.data)
+    setModalActive(true)
+}
+
+
+const handleChange = (e) => {
+    console.log(e.target.checked);
+}
+    
+
+const contentForPopUp = (compList, addedCompToList) => {
+    {console.log('Здесь проблема ', compList);}
     return (
         <div>
-            <p>
-                lorem23
-            </p>
+            <h3>Выбери ПК</h3>
+                   <div>
+                    {  compList.computerName.map((compName, index) => <p key={index}>
+                        <input onClick={(e) => addedCompToList(e.target.dataset.set)} 
+                        type="checkbox" 
+                        data-set={compList.DistinguishedName[index]} />{compName}</p>
+                    )
+                       
+                }   
+                </div>
+                    <button onClick={handleChange}>К выбору софта</button>
         </div>
     )
 }
-
-export default InstallSoft
